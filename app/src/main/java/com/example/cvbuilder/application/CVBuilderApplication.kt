53c2b916eb711ac1d3e39b300db2a694e5b20db6
Mic542 <mch.9975@gmail.com>
2022-03-26 21:50:05 +0900

    Fresco initializer

--- app/src/main/java/com/example/cvbuilder/application/CVBuilderApplication.kt
+++ app/src/main/java/com/example/cvbuilder/application/CVBuilderApplication.kt
@@ -1,7 +1,14 @@
 package com.example.cvbuilder.application
 
 import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.hilt.android.HiltAndroidApp
 
+
 @HiltAndroidApp
-class CVBuilderApplication : Application() { }
+class CVBuilderApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Fresco.initialize(this)
+    }
+}
