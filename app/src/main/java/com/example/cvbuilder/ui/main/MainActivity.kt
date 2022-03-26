a828d2e965e8fc2b4b7ef9eefd3c1aa9ae42b3d9
Mic542 <mch.9975@gmail.com>
2022-03-26 21:49:44 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/cvbuilder/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/cvbuilder/ui/main/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.cvbuilder.ui.main
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.cvbuilder.R
 import com.example.cvbuilder.viewmodel.CVViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val cvViewModel : CVViewModel by viewModels()
+    val cvViewModel : CVViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        supportActionBar?.title = "Resume/CV List"
     }
 }
