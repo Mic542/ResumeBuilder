25ec22cefabfe19fea7db326dc42c7c571444820
Mic542 <mch.9975@gmail.com>
2022-04-01 03:03:59 +0900

    add getAll not null test case

--- app/src/test/java/com/example/cvbuilder/viewmodel/CVViewModelTest.kt
+++ app/src/test/java/com/example/cvbuilder/viewmodel/CVViewModelTest.kt
@@ -1,26 +1,62 @@
 package com.example.cvbuilder.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.asLiveData
+import com.example.cvbuilder.data.CVData
 import com.example.cvbuilder.data.dao.BaseCVDao
 import com.example.cvbuilder.repository.CVRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.kotlin.mock
+import java.util.*
 
 class CVViewModelTest {
 
+    @get:Rule
+    var rule = InstantTaskExecutorRule()
     @Mock
     lateinit var repository : CVRepository
     @Mock
     lateinit var dao : BaseCVDao
-    lateinit var cvViewModel: CVViewModel
+    @Mock
+    private lateinit var cvViewModel: CVViewModel
+    @Mock
+    private lateinit var cv : Flow<List<CVData>>
 
     @Before
     fun setUp() {
         dao = mock()
         repository = mock()
         cvViewModel = CVViewModel(repository)
+            cv = flow<List<CVData>>{
+                listOf(CVData(
+                0,
+                "",
+                "",
+                "John",
+                "",
+                "",
+                "",
+                "",
+                0,
+                null,
+                null,
+                null,
+                null)
+            )
+        }
+        Mockito.`when`(repository.getAll()).thenReturn(cv)
     }
 
     @Test
@@ -39,5 +75,11 @@ class CVViewModelTest {
     fun `set isNew to false`() {
         cvViewModel.isNew = false
         Assert.assertFalse(cvViewModel.isNew)
+    }
+
+    @Test
+    fun `getAll not null`() = runBlocking {
+        val result = cvViewModel.getAll()
+        Assert.assertNotNull(result)
     }
 }
